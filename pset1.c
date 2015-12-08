6454a077b49963f805cef7816ee58d0be38e2581
ashzor1337 <ashley.dean.wright@gmail.com>
2015-12-08 19:48:58 +0000

    Create pset1.c

---
+++ pset1.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+   int v; 
+   
+    /// ask for user input, then validate.///
+    
+        do printf("please give me a value between 0-23:\n"); 
+        }
+        v =GetInt();
+        {
+        while (v > 0 && v <=23);
+    
+}
+    
